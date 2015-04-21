c5f4119630e7809d0bcc8210d788a24e92f03072
Brian Feldstein <bfeldste@gmail.com>
2015-04-21 15:56:00 -0400

    reqs fixed51

--- tweet_sense.py
+++ tweet_sense.py
@@ -103,4 +103,6 @@ def validpage():
 #    return render_template('emails.html', email_addresses=email_addresses)
 
 if __name__ == "__main__":
-    app.run(debug = False)
+#    app.run(debug = False)
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host = '0.0.0.0', port=port)
